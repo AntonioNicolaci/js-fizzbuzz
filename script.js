a1171fc3689a0009e4f0a48d23ab88cd0bade267
antonionicolaci <antonionicolaci48@gmail.com>
2023-04-14 14:03:52 +0200

    add FizzBuzz algoritm

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-fizzbuzz()
-{
-    for (let i = 0; i < 100; i++) {
-        console.log(i)
+function fizzbuzz(){
+    for (let i = 1; i <= 100; i++) {
+        
+        if([i % 3] == 0){
+            console.log("Fizz");
+        }
+        else if ([i % 5] == 0){
+            console.log("Buzz");
+        }
+        else{
+            console.log(i);
+        }
     }
 }
