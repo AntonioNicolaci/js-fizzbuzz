acdeeb94a192e0c6f97202e34a0e0b6b26e95846
antonionicolaci <antonionicolaci48@gmail.com>
2023-04-14 15:30:42 +0200

    add bonus 1 & 2

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-function fizzbuzz(){
+let text_div;
+
     for (let i = 1; i <= 100; i++) {
-        
         if ([i % 15] == 0){
-            console.log("FizBuzz")
+            text_div = "FizzBuzz";
+            document.getElementById(i).innerHTML = `<div class="casella ${text_div}">${text_div}</div>`;
+            text_div = "";
+            console.log(i)
         }
         else if([i % 3] == 0){
-            console.log("Fizz");
+            text_div = "Fizz";
+            document.getElementById(i).innerHTML = `<div class="casella ${text_div}">${text_div}</div>`;
+            text_div = "";
+            console.log(i)
         }
         else if ([i % 5] == 0){
-            console.log("Buzz");
+            text_div = "Buzz";
+            document.getElementById(i).innerHTML = `<div class="casella ${text_div}">${text_div}</div>`;
+            text_div = "";
+            console.log(i);
         }
         else{
+            text_div = i;
+            document.getElementById(i).innerHTML = `<div class="casella number">${text_div}</div>`;
+            text_div = "";
             console.log(i);
         }
     }
-}
