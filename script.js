a6fd53aca7de9e63e351852b8bf49503a04d1c16
antonionicolaci <antonionicolaci48@gmail.com>
2023-04-14 15:42:10 +0200

    optimaze script for add div in one action

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let text_div, text_for, text_table = "";
     for (let i = 1; i <= 100; i++) {
         if ([i % 15] == 0){
             text_div = "FizzBuzz";
-            // document.getElementById(i).innerHTML = `<div class="casella ${text_div}">${text_div}</div>`;
             text_for = `<div class="casella ${text_div}">${text_div}</div>`;
             text_table = text_table + text_for;
             text_div = "";
@@ -12,7 +11,6 @@ let text_div, text_for, text_table = "";
         }
         else if([i % 3] == 0){
             text_div = "Fizz";
-            // document.getElementById(i).innerHTML = `<div class="casella ${text_div}">${text_div}</div>`;
             text_for = `<div class="casella ${text_div}">${text_div}</div>`;
             text_table = text_table + text_for;
             text_div = "";
@@ -21,7 +19,6 @@ let text_div, text_for, text_table = "";
         }
         else if ([i % 5] == 0){
             text_div = "Buzz";
-            // document.getElementById(i).innerHTML = `<div class="casella ${text_div}">${text_div}</div>`;
             text_for = `<div class="casella ${text_div}">${text_div}</div>`;
             text_table = text_table + text_for;
             text_div = "";
@@ -30,7 +27,6 @@ let text_div, text_for, text_table = "";
         }
         else{
             text_div = i;
-            // document.getElementById(i).innerHTML = `<div class="casella number">${text_div}</div>`;
             text_for = `<div class="casella number">${text_div}</div>`;
             text_table = text_table + text_for;
             text_div = "";
